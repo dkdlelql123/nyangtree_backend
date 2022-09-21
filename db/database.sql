0c6d270d07ccbb631061212dfc983c863897b399
NamYeJi <dkdlelql123@naver.com>
2022-09-21 11:27:21 +0900

    회원 테이블 생성 및 관리자, 테스트 계정 생성

---
+++ db/database.sql
@@ -0,0 +1,38 @@
+
+# 회원테이블 생성
+CREATE TABLE `member`(
+	id INT(10) PRIMARY KEY AUTO_INCREMENT,
+	userId VARCHAR(20) NOT NULL UNIQUE,
+	userPw VARCHAR(50) NOT NULL,
+	username VARCHAR(20),
+	nickname VARCHAR(30) NOT NULL UNIQUE,
+	phone VARCHAR(20),
+	email VARCHAR(50) NOT NULL,
+	authLevel SMALLINT(2) NOT NULL DEFAULT 2 COMMENT '2:일반회원, 7:관리자',
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	delStatus TINYINT(1) DEFAULT 0 COMMENT '0:탈퇴전, 1:탈퇴',
+	delDate DATETIME
+);
+
+# 관리자 계정 생성
+INSERT INTO `member`
+SET userId = 'admin',
+userPw = '1234',
+username = '관리자',
+nickname = 'manager',
+email = 'dkdlelql123@naver.com',
+authLevel = 7,
+regDate = NOW(),
+updateDate = NOW();
+
+# 일반계정 생성
+INSERT INTO `member`
+SET userId = 'test',
+userPw = '1234',
+username = '홍길동',
+nickname = '둘리아빠',
+email = 'dkdlelql123@naver.com',
+authLevel = 2,
+regDate = NOW(),
+updateDate = NOW();
