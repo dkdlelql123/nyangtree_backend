99ae056b2ef52917e2e15892dcfd0f3b18910001
NamYeJi <dkdlelql123@naver.com>
2022-09-21 09:56:22 +0900

    백엔드 작업 - mysql 연동완료

---
+++ src/main/java/com/nyj/exam/demo/BoardBackApplication.java
@@ -0,0 +1,20 @@
+package com.nyj.exam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class BoardBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardBackApplication.class, args);
+	}
+
+	@RequestMapping("/hi")
+	public String hi() {
+		return "hi~~~";
+	}
+}
---
+++ src/main/java/com/nyj/exam/demo/controller/TestService.java
@@ -0,0 +1,23 @@
+package com.nyj.exam.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+//@RequestMapping("/api")
+public class TestService {
+ 
+	
+	@GetMapping("/api/hello")
+	public String getHello() {
+		return "hello";
+	}
+	
+	@GetMapping("/api/member/list")
+	public String getMemberList() {
+//		List<Member> memberList = memberService.getMemberList();
+		return "hello";
+	}
+}
